9f216bb3c7153af85446bbf846c7e573219976f4
Amadeus398 <digger_e398@mail.ru>
2022-06-03 23:29:04 +0300

    feat[otus_scripts] create php install script

---
+++ 4.06.2022/php_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ROOT_UID=0
+
+if [ "$UID" -ne "$ROOT_UID" ]; then
+    echo "Only a user with root privileges can use this command"
+    exit 1
+fi
+
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
+
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm && \
+yum install -y epel-release yum-utils && \
+yum-config-manager --disable remi-php54 && \
+yum-config-manager --enable remi-php73 && \
+yum update -y && \
+
+yum install -y php php-cli php-fpm php-mysqlnd \
+php-zip php-devel php-gd php-mcrypt php-mbstring \
+php-curl php-xml php-pear php-bcmath php-json && \
+
+echo "php 7.3 installed successfully" && \
+exit 0
