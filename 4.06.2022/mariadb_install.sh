cbbfc136d95e1093f0c38d7b2fd2a407a9f3a6e2
Amadeus398 <digger_e398@mail.ru>
2022-06-03 23:43:47 +0300

    feat[otus_scripts] create mariadb install script

---
+++ 4.06.2022/mariadb_install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+ROOT_UID=0
+
+if [ "$UID" -ne "$ROOT_UID" ]; then
+    echo "Only a user with root privileges can use this command"
+    exit 1
+fi
+
+yum -y install mariadb-server mariadb && \
+systemctl start mariadb && \
+mysql_secure_installation && \
+
+sed -i "s/\[server\]/[server]\nserver-id = 10/" /etc/my.cnf.d/server.cnf && \
+
+systemctl restart mariadb && \
+
+echo "mariadb installed successfully" && \
+exit 0
