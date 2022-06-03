70e458c7415d5ee1e1c37958010af4100e9be1f7
Amadeus398 <digger_e398@mail.ru>
2022-06-03 23:35:36 +0300

    feat[otus_scripts] edit apache and wordpress install script

---
+++ 4.06.2022/apache_wp_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ADDR=$1
+ROOT_UID=0
+
+if [ "$UID" -ne "$ROOT_UID" ]; then
+    echo "Only a user with root privileges can use this command"
+    exit 1
+fi
+
+
+yum update -y httpd && sudo yum install -y httpd && \
+
+rsync -avz "$ADDR":/etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf && \
+rsync -avz "$ADDR":/var/www/html/wordpress /var/www/html/ && \
+
+mkdir -p /var/www/html/wp-content/uploads && \
+
+chown -R apache:apache /var/www/html/* && \
+
+systemctl enable --now httpd.service && \
+systemctl status httpd.service
+
+
